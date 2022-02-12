a8950347e4fad5a65d4d79b4097651402528b1b3
harveyjin <harveyjin@tencent.com>
2022-02-12 21:55:54 +0800

    首次提交

---
+++ Assets/MemoryOrderingTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using DefaultNamespace;
+
+public class MemoryOrderingTest : MonoBehaviour
+{
+    public Button StartBtn;
+    public InputField RoundField;
+    public InputField PairCountField;
+    public Text Text;
+    public Toggle Toggle;
+
+    private int memoryBarrier;
+    private readonly List<TestPair> mTestPairs = new List<TestPair>();
+
+    private void Start()
+    {
+        StartBtn.onClick.AddListener(OnClick);
+    }
+
+    private void Update()
+    {
+        int curRound = 0;
+        int maxRound = 0;
+        int error = 0;
+        foreach (var testPair in mTestPairs)
+        {
+            curRound += testPair.curRound;
+            maxRound += testPair.maxRound;
+            error += testPair.error;
+        }
+        int progress = maxRound == 0 ? 0 : (int)((double)curRound / maxRound * 100);
+        
+        Text.text = $"Memory Barrier: {memoryBarrier}\nRound: {progress}\nError: {error}";
+    }
+
+    private void OnClick()
+    {
+        memoryBarrier = Toggle.isOn ? 1 : 0;
+        
+        mTestPairs.Clear();
+        for (var i = 0; i < int.Parse(PairCountField.text); ++i)
+        {
+            var testPair = new TestPair();
+            testPair.Start(memoryBarrier, int.Parse(RoundField.text));
+            mTestPairs.Add(testPair);
+        }
+    }
+}
---
+++ Assets/TestPair.cs
@@ -0,0 +1,74 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace DefaultNamespace
+{
+    public class TestPair
+    {
+        public int running;
+        public int memoryBarrier;
+        public int curRound;
+        public int maxRound;
+        public int error;
+        public int a;
+        public int b;
+        public int c;
+        public int d;
+
+        public void Start(int memoryBarrier, int maxRound)
+        {
+            a = 0;
+            b = 0;
+            c = 0;
+            d = 0;
+            running = 1;
+            this.memoryBarrier = memoryBarrier;
+            curRound = 0;
+            this.maxRound = maxRound;
+
+            Task.Run(Foo1);
+            Task.Run(Foo2);
+        }
+        
+        private void Foo1()
+        {
+            for (int i = 0; i < maxRound; ++i)
+            {
+                a = 1;
+                if (memoryBarrier == 1) Thread.MemoryBarrier();
+                b = 1;
+
+                while (d == 0) {}
+                if (c != 1)
+                    Interlocked.Increment(ref error);
+                c = 0;
+                d = 0;
+
+                curRound = i + 1;
+                Thread.Sleep(1);
+            }
+
+            a = 1;
+            if (memoryBarrier == 1) Thread.MemoryBarrier();
+            b = 1;
+        
+            running = 0;
+        }
+
+        private void Foo2()
+        {
+            while (running == 1)
+            {
+                while (b == 0) {}
+                if (a != 1)
+                    Interlocked.Increment(ref error);
+                a = 0;
+                b = 0;
+
+                c = 1;
+                if (memoryBarrier == 1) Thread.MemoryBarrier();
+                d = 1;
+            }
+        }
+    }
+}
